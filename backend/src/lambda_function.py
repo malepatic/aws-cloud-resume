3ebeac97793b06a2880658d4efc654ccc884565f
malepatic <malepati.c@northeastern.edu>
2025-04-14 17:41:42 -0400

    added lambda function

--- backend/src/lambda_function.py
+++ backend/src/lambda_function.py
@@ -1,13 +1,12 @@
 import boto3
 import json
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('visitorCounter')
-
 def lambda_handler(event, context):
-    # Example of handling a GET request to retrieve data from DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('visitorCounter')
+
     response = table.get_item(Key={'id': event['pathParameters']['id']})
-    
+
     if 'Item' in response:
         return {
             'statusCode': 200,

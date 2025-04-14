9e272e0c6f81de0fb5caf2d800b04a4944704405
malepatic <malepati.c@northeastern.edu>
2025-04-14 19:45:57 -0400

    added frontend

---
+++ frontend/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "acm_cert_arn" {
+  type = string
+  default = "arn:aws:acm:us-east-1:774305571847:certificate/7faa2a4f-69cc-424e-993b-4db017273131"
+  
+}
